9110c7ab922cbcd2ad358206e831be3edb669b01
Sweeney, Mack <mackenzie.sweeney@capitalone.com>
2018-02-04 14:53:30 -0500

    Big ol' refactor to clarify and modularize code.

--- tikzmagic.py
+++ tikzmagic.py
@@ -29,20 +29,17 @@ Usage
 
 from __future__ import print_function
 
-
 import os
 import sys
+import shutil
 import tempfile
 import contextlib
-from shutil import rmtree, copy
-from subprocess import call
+import subprocess
 from xml.dom import minidom
 
 from IPython.core.displaypub import publish_display_data
-from IPython.core.magic import (Magics, magics_class, line_cell_magic, needs_local_scope)
-from IPython.core.magic_arguments import (
-    argument, magic_arguments, parse_argstring
-)
+from IPython.core.magic import Magics, magics_class, line_cell_magic, needs_local_scope
+from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
 from IPython.testing.skipdoctest import skip_doctest
 
 try:
@@ -59,6 +56,10 @@ _MIME_TYPES = {
 }
 
 
+def get_mime_type(img_format):
+    return _MIME_TYPES.get(img_format, 'image/%s' % img_format)
+
+
 def split_csv_args(arg_string):
     return [name.strip()
             for name in arg_string.strip().split(',')
@@ -73,8 +74,124 @@ def working_directory(path):
     """
     prev_cwd = os.getcwd()
     os.chdir(path)
-    yield prev_cwd
-    os.chdir(prev_cwd)
+    try:
+        yield prev_cwd
+    finally:
+        os.chdir(prev_cwd)
+
+
+@contextlib.contextmanager
+def make_tempdir():
+    """This sort of thing exists in Py3 as `tempfile.TemporaryDirectory`,
+    but we roll our own here for Py2 compatibility.
+    """
+    tempdir_path = tempfile.mkdtemp().replace('\\', '/')
+    try:
+        yield tempdir_path
+    finally:
+        shutil.rmtree(tempdir_path)
+
+
+def run_latex(code, dirpath, encoding='utf-8'):
+    with open(dirpath + '/tikz.tex', 'w', encoding=encoding) as f:
+        f.write(code)
+
+    with working_directory(dirpath) as current_dir:
+        # in case of error return LaTeX log
+        if not _convert_tikz_latex(current_dir):
+            return _read_tikz_log()
+
+
+def _convert_tikz_latex(current_dir):
+    """Returns True if conversion is successful, else False."""
+    # Set the TEXINPUTS environment variable, which allows the tikz code
+    # to reference files relative to the notebook (includes, packages, ...)
+    env = os.environ.copy()
+    if 'TEXINPUTS' in env:
+        env['TEXINPUTS'] = current_dir + os.pathsep + env['TEXINPUTS']
+    else:
+        env['TEXINPUTS'] = '.' + os.pathsep + current_dir + os.pathsep * 2
+        # note that the trailing double pathsep will insert the standard
+        # search path (otherwise we would lose access to all packages)
+
+    try:
+        retcode = subprocess.call(
+            "pdflatex --shell-escape tikz.tex", shell=True, env=env)
+
+        if retcode != 0:
+            print("LaTeX terminated with signal", -retcode, file=sys.stderr)
+            return False
+    except OSError as e:
+        print("LaTeX execution failed:", e, file=sys.stderr)
+        return False
+
+    return True
+
+
+def _read_tikz_log(log_path='tikz.log', encoding='latin-1'):
+    """Returns log from `tikz.log` if that file exists, else None."""
+    try:
+        with open(log_path, 'r', encoding=encoding) as f:
+            return f.read()
+    except IOError:
+        print("No log file generated.", file=sys.stderr)
+        return None
+
+
+def _convert_img_format(plot_dir, plot_format):
+    if plot_format == 'jpg' or plot_format == 'jpeg':
+        _convert_png_to_jpg(plot_dir)
+    elif plot_format == 'svg':
+        _convert_pdf_to_svg(plot_dir)
+
+
+def _convert_pdf_to_svg(dirpath):
+    with working_directory(dirpath):
+        try:
+            retcode = subprocess.call("pdf2svg tikz.pdf tikz.svg", shell=True)
+            if retcode != 0:
+                print("pdf2svg terminated with signal", -retcode, file=sys.stderr)
+        except OSError as e:
+            print("pdf2svg execution failed:", e, file=sys.stderr)
+
+
+def _convert_png_to_jpg(dirpath):
+    with working_directory(dirpath):
+        try:
+            retcode = subprocess.call(
+                "convert tikz.png -quality 100 -background white -flatten tikz.jpg",
+                shell=True)
+
+            if retcode != 0:
+                print("convert terminated with signal", -retcode, file=sys.stderr)
+        except OSError as e:
+            print("convert execution failed:", e, file=sys.stderr)
+
+
+def _fix_gnuplot_svg_size(image, size=None):
+    """GnuPlot SVGs do not have height/width attributes. Set
+    these to be the same as the viewBox, so that the browser
+    scales the image correctly.
+
+    Parameters
+    ----------
+    image : str|bytes
+        SVG data.
+    size : tuple of int
+        Image width, height.
+
+    """
+    (svg,) = minidom.parseString(image).getElementsByTagName('svg')
+    viewbox = svg.getAttribute('viewBox').split(' ')
+
+    if size is not None:
+        width, height = size
+    else:
+        width, height = viewbox[2:]
+
+    svg.setAttribute('width', '%dpx' % width)
+    svg.setAttribute('height', '%dpx' % height)
+    return svg.toxml()
 
 
 @magics_class
@@ -89,140 +206,39 @@ class TikzMagics(Magics):
 
         """
         super(TikzMagics, self).__init__(shell)
-        self._plot_format = 'png'
-
-        # Allow publish_display_data to be overridden for
-        # testing purposes.
-        self._publish_display_data = publish_display_data
-
-    def _fix_gnuplot_svg_size(self, image, size=None):
-        """GnuPlot SVGs do not have height/width attributes. Set
-        these to be the same as the viewBox, so that the browser
-        scales the image correctly.
-
-        Parameters
-        ----------
-        image : str
-            SVG data.
-        size : tuple of int
-            Image width, height.
-
-        """
-        (svg,) = minidom.parseString(image).getElementsByTagName('svg')
-        viewbox = svg.getAttribute('viewBox').split(' ')
-
-        if size is not None:
-            width, height = size
-        else:
-            width, height = viewbox[2:]
-
-        svg.setAttribute('width', '%dpx' % width)
-        svg.setAttribute('height', '%dpx' % height)
-        return svg.toxml()
-
-    def _run_latex(self, code, encoding, dir):
-        with open(dir + '/tikz.tex', 'w', encoding=encoding) as f:
-            f.write(code)
-
-        with working_directory(dir) as current_dir:
-            # in case of error return LaTeX log
-            if not self._convert_tikz_latex(current_dir):
-                return self._read_tikz_log()
-
-    def _convert_tikz_latex(self, current_dir):
-        """Returns True if conversion is successful, else False."""
-        # Set the TEXINPUTS environment variable, which allows the tikz code
-        # to reference files relative to the notebook (includes, packages, ...)
-        env = os.environ.copy()
-        if 'TEXINPUTS' in env:
-            env['TEXINPUTS'] = current_dir + os.pathsep + env['TEXINPUTS']
-        else:
-            env['TEXINPUTS'] = '.' + os.pathsep + current_dir + os.pathsep * 2
-            # note that the trailing double pathsep will insert the standard
-            # search path (otherwise we would lose access to all packages)
-
-        try:
-            retcode = call("pdflatex --shell-escape tikz.tex", shell=True, env=env)
-            if retcode != 0:
-                print("LaTeX terminated with signal", -retcode, file=sys.stderr)
-                return False
-        except OSError as e:
-            print("LaTeX execution failed:", e, file=sys.stderr)
-            return False
-
-        return True
-
-    def _read_tikz_log(self):
-        """Returns log from `tikz.log` if that file exists, else None."""
-        try:
-            with open('tikz.log', 'r', encoding='latin-1') as f:
-                return f.read()
-        except IOError:
-            print("No log file generated.", file=sys.stderr)
-            return None
-
-    def _convert_pdf_to_svg(self, dir):
-        with working_directory(dir):
-            try:
-                retcode = call("pdf2svg tikz.pdf tikz.svg", shell=True)
-                if retcode != 0:
-                    print("pdf2svg terminated with signal", -retcode, file=sys.stderr)
-            except OSError as e:
-                print("pdf2svg execution failed:", e, file=sys.stderr)
-
-    def _convert_png_to_jpg(self, dir):
-        with working_directory(dir):
-            try:
-                retcode = call("convert tikz.png -quality 100 -background white -flatten tikz.jpg",
-                               shell=True)
-                if retcode != 0:
-                    print("convert terminated with signal", -retcode, file=sys.stderr)
-            except OSError as e:
-                print("convert execution failed:", e, file=sys.stderr)
 
     @skip_doctest
     @magic_arguments()
     @argument(
         '-sc', '--scale', action='store', type=str, default=1,
-        help='Scaling factor of plots. Default is "--scale 1".'
-        )
+        help='Scaling factor of plots. Default is "--scale 1".')
     @argument(
         '-s', '--size', action='store', type=str, default='400,240',
-        help='Pixel size of plots, "width,height". Default is "--size 400,240".'
-        )
+        help='Pixel size of plots, "width,height". Default is "--size 400,240".')
     @argument(
         '-f', '--format', action='store', type=str, default='png',
-        help='Plot format (png, svg or jpg).'
-        )
+        help='Plot format (png, svg or jpg).')
     @argument(
         '-e', '--encoding', action='store', type=str, default='utf-8',
-        help='Text encoding, e.g., -e utf-8.'
-        )
+        help='Text encoding, e.g., -e utf-8.')
     @argument(
         '-x', '--preamble', action='store', type=str, default='',
-        help='LaTeX preamble to insert before tikz figure, e.g., -x $preamble, with preamble some string variable.'
-        )
+        help='LaTeX preamble to insert before tikz figure, e.g., '
+             '-x $preamble, with preamble some string variable.')
     @argument(
         '-p', '--package', action='store', type=str, default='',
-        help='LaTeX packages to load, separated by comma, e.g., -p pgfplots,textcomp.'
-        )
+        help='LaTeX packages to load, separated by comma, e.g., -p pgfplots,textcomp.')
     @argument(
         '-l', '--library', action='store', type=str, default='',
-        help='TikZ libraries to load, separated by comma, e.g., -l matrix,arrows.'
-        )
+        help='TikZ libraries to load, separated by comma, e.g., -l matrix,arrows.')
     @argument(
         '-S', '--save', action='store', type=str, default=None,
-        help='Save a copy to file, e.g., -S filename. Default is None'
-        )
+        help='Save a copy to file, e.g., -S filename. Default is None')
     @argument(
         '-d', '--dry-run', action='store_true', default=False,
-        help='Output the LaTeX code that will be generated.'
-    )
+        help='Output the LaTeX code that will be generated.')
     @needs_local_scope
-    @argument(
-        'code',
-        nargs='*',
-        )
+    @argument('code', nargs='*')
     @line_cell_magic
     def tikz(self, line, cell=None, local_ns=None):
         """Run TikZ code in LaTeX and plot result.
@@ -255,120 +271,144 @@ class TikzMagics(Magics):
                 ...: (m-1-2) edge node [right] {$bd$} (m-2-2)
                 ...: (m-2-1) edge node [below] {$cd$} (m-2-2);
         """
-
-        # read arguments
         args = parse_argstring(self.tikz, line)
-        scale = args.scale
-        size = args.size
-        width, height = split_csv_args(size)
-        plot_format = args.format
-        encoding = args.encoding
-        preamble = args.preamble
-        tikz_libraries = split_csv_args(args.library)
-        latex_package = split_csv_args(args.package)
-        dry_run = args.dry_run
 
-        # arguments 'code' in line are prepended to the cell lines
+        # If code blocks are passed in args, prepend to the block in the cell.
+        code_from_args = str('\n').join(
+            section.strip("'\"")
+            for section in args.code
+        ).strip()
+
         if cell is None:
-            code = ''
-            return_output = True
+            code = code_from_args
         else:
-            code = cell
-            return_output = False
+            code = code_from_args + cell
 
-        code = str('').join(args.code) + code
+        runner = TikzRunner(
+            code, args.package, args.library, args.preamble, args.size, args.scale,
+            plot_format=args.format, encoding=args.encoding,
+            img_save_path=args.save, dry_run=args.dry_run)
+        runner.run()
 
-        # if there is no local namespace then default to an empty dict
-        if local_ns is None:
-            local_ns = {}
 
-        # generate plots in a temporary directory
-        plot_dir = tempfile.mkdtemp().replace('\\', '/')
+class TikzRunner(object):
+    """Run Tikz code to compile images."""
 
+    def __init__(self, code, latex_packages, tikz_libraries, preamble, size, scale,
+                 plot_format='svg', encoding='utf-8', img_save_path='', dry_run=False):
+        self.code = code
+        self.tikz_libraries = split_csv_args(tikz_libraries)
+        self.latex_packages = split_csv_args(latex_packages)
+        self.preamble = preamble
+        self.width, self.height = map(int, split_csv_args(size))
+        self.scale = scale
+
+        self.plot_format = plot_format
+        self.encoding = encoding
+        self.img_save_path = img_save_path
+        self.dry_run = dry_run
+
+        self._key = 'TikZMagic.Tikz'
+
+    def run(self):
+        compiled_code = self.compile_tikz_template()
+        if self.dry_run:
+            print(compiled_code)
+        else:
+            self._run_and_display(compiled_code)
+
+    def _run_and_display(self, compiled_code):
+        display_data = self.generate_plots(compiled_code)
+        for tag, disp_d in display_data:
+            if self.plot_format == 'svg':
+                # isolate data in an iframe, to prevent clashing glyph declarations in SVG
+                publish_display_data(source=tag, data=disp_d, metadata={'isolated': 'true'})
+            else:
+                publish_display_data(source=tag, data=disp_d, metadata=None)
+
+    def compile_tikz_template(self):
         add_params = ""
-        if plot_format == 'png' or plot_format == 'jpg' or plot_format == 'jpeg':
+        if self.plot_format in {'png', 'jpg', 'jpeg'}:
             add_params += "density=300,"
 
-        tex = ['''
-\\documentclass[convert={%(add_params)ssize=%(width)sx%(height)s,outext=.png},border=0pt]{standalone}
-\\usepackage{tikz}''' % locals()]
+        kwargs = {
+            'add_params': add_params,
+            'width': self.width,
+            'height': self.height,
+            'scale': self.scale,
+        }
 
-        for pkg in latex_package:
-            tex.append('''
-\\usepackage{%s}''' % pkg)
+        convert_args = '%(add_params)ssize=%(width)sx%(height)s,outext=.png' % kwargs
+        tex = ['\\documentclass[convert={%s},border=0pt]{standalone}' % convert_args,
+               '\\usepackage{tikz}']
 
-        tex.append('''
-\\usetikzlibrary{%s}''' % ','.join(tikz_libraries))
+        for pkg in self.latex_packages:
+            tex.append('\\usepackage{%s}' % pkg)
 
-        if preamble is not None:
-            tex.append('''
-%s''' % preamble.strip("'\""))  # allows users to string-escape spacing
+        tex.append('\\usetikzlibrary{%s}' % ','.join(self.tikz_libraries))
 
-        tex.append('''
-\\begin{document}
-\\begin{tikzpicture}[scale=%(scale)s]''' % locals())
+        if self.preamble is not None:
+            # the strip allows users to string-escape spacing
+            tex.append('%s' % self.preamble.strip("'\""))
+
+        tex.append('\\begin{document}\n'
+                   '\\begin{tikzpicture}[scale=%(scale)s]' % kwargs)
 
         tex.append('\n'.join([
             '    %s' % line.strip()
-            for line in code.split(os.linesep)
+            for line in self.code.split(os.linesep)
         ]))
 
-        tex.append('''
-\\end{tikzpicture}
-\\end{document}
-        ''')
+        tex.append('\\end{tikzpicture}\n'
+                   '\\end{document}')
 
-        code = str('').join(tex)
-        if dry_run:
-            print(code)
-            return
+        return str('\n').join(tex)
 
-        latex_log = self._run_latex(code, encoding, plot_dir)
-
-        key = 'TikZMagic.Tikz'
+    def generate_plots(self, compiled_code):
         display_data = []
+        with make_tempdir() as plot_dir:
+            latex_log = run_latex(compiled_code, plot_dir, self.encoding)
 
-        # If the latex error log exists, then image generation has failed.
-        # Publish error log and return immediately
-        if latex_log:
-            self._publish_display_data(source=key, data={'text/plain': latex_log})
-            return
+            # If the latex error log exists, then image generation has failed.
+            # Publish error log and return immediately
+            if latex_log:
+                publish_display_data(
+                    source=self._key,
+                    data={'text/plain': latex_log})
+            else:
+                _convert_img_format(plot_dir, self.plot_format)
 
-        if plot_format == 'jpg' or plot_format == 'jpeg':
-            self._convert_png_to_jpg(plot_dir)
-        elif plot_format == 'svg':
-            self._convert_pdf_to_svg(plot_dir)
+                image_filename = "%s/tikz.%s" % (plot_dir, self.plot_format)
+                img = self._publish_image(image_filename)
+                if img is not None:
+                    display_data.append((self._key, img))
 
-        image_filename = "%s/tikz.%s" % (plot_dir, plot_format)
+                self._save_if_requested(image_filename)
 
-        # Publish image
+        return display_data
+
+    def _publish_image(self, image_filename):
         try:
-            image = open(image_filename, 'rb').read()
-            plot_mime_type = _MIME_TYPES.get(plot_format, 'image/%s' % (plot_format))
-            width, height = [int(s) for s in size.split(',')]
-            if plot_format == 'svg':
-                image = self._fix_gnuplot_svg_size(image, size=(width, height))
+            with open(image_filename, 'rb') as f:
+                image = f.read()
 
-            display_data.append((key, {plot_mime_type: image}))
+            plot_mime_type = get_mime_type(self.plot_format)
+            if self.plot_format == 'svg':
+                image = _fix_gnuplot_svg_size(image, size=(self.width, self.height))
+
+            return {plot_mime_type: image}
         except IOError:
             print("No image generated.", file=sys.stderr)
+            return None
 
-        # Copy output file if requested
-        if args.save is not None:
-            copy(image_filename, args.save)
-
-        rmtree(plot_dir)
-
-        for tag, disp_d in display_data:
-            if plot_format == 'svg':
-                # isolate data in an iframe, to prevent clashing glyph declarations in SVG
-                self._publish_display_data(source=tag, data=disp_d, metadata={'isolated': 'true'})
-            else:
-                self._publish_display_data(source=tag, data=disp_d, metadata=None)
+    def _save_if_requested(self, image_filename):
+        """Copy output file if requested."""
+        if self.img_save_path is not None:
+            shutil.copy(image_filename, self.img_save_path)
 
 
 __doc__ = __doc__.format(
-    TIKZ_DOC = ' '*8 + TikzMagics.tikz.__doc__,
+    TIKZ_DOC=' ' * 8 + TikzMagics.tikz.__doc__,
 )
 
 
